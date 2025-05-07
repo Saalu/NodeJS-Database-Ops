55bfa161909ccf292f85e027b45f2ede8dfaf374
saalu issaka <saaluissaka@gmail.com>
2025-05-07 14:02:47 +0000

    Added a json funtion to interact with api

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const studentRouter = require('./router/studentRouter')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-
+app.use(express.json())
 app.use('/api/students', studentRouter)
+app.use('/', (req,res) => {
+    // res.json({msg: "Hello Nodejs World"})
+    console.log("Test all now")
+
+
+} )
 
 
 app.listen( () => {
     console.log(`Server started on port: ${PORT}`)
 })
-
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected"))
